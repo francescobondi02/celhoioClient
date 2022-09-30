834bb91572b3589bf31bf69e14af8269462efdd3
Francesco Bondi <francesco.bondi02@gmail.com>
2022-09-30 22:35:32 +0200

    chat error

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -22,8 +22,8 @@ import ChatMessage from "./ChatMessage";
 import { UserContext } from "../../user-context";
 
 //Da cambiare
-const socket = io.connect("http://localhost:3001");
-//const socket = io.connect("https://celhoio.herokuapp.com");
+//const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://celhoio.herokuapp.com");
 
 export default function Chat() {
   const { user, handleUser } = useContext(UserContext);
