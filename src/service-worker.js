843ee15fbe51430a5f3d65196bfe05de943c35cf
Francesco Bondi <francesco.bondi02@gmail.com>
2022-10-09 19:36:51 +0200

    CHANGED SW

--- src/service-worker.js
+++ src/service-worker.js
@@ -86,11 +86,11 @@ self.addEventListener("push", (e) => {
       primaryKey: 2,
     },
     actions: [
-      {
+      /*{
         action: "open",
         title: data.action1,
         icon: "images/checkmark.png",
-      },
+      },*/
       {
         action: "close",
         title: "Chiudi",
@@ -103,15 +103,31 @@ self.addEventListener("push", (e) => {
 
 self.addEventListener(
   "notificationclick",
-  function (event) {
-    //var messageId = event.notification.data;
+  function (e) {
+    // Close the notification popout
+    e.notification.close();
+    // Get all the Window clients
 
-    event.notification.close();
+    e.waitUntil(
+      clients.matchAll({ type: "window" }).then((clientsArr) => {
+        // If a Window tab matching the targeted URL already exists, focus that;
+        const hadWindowToFocus = clientsArr.some((windowClient) =>
+          windowClient.url === e.notification.data.url
+            ? (windowClient.focus(), true)
+            : false
+        );
+        // Otherwise, open a new tab to the applicable URL and focus it.
+        if (!hadWindowToFocus)
+          clients
+            .openWindow(e.notification.data.url)
+            .then((windowClient) =>
+              windowClient ? windowClient.focus() : null
+            );
+      })
+    );
 
-    if (event.action == "open") {
-      clients.openWindow("https://www.celhoio.it");
-    } else if (event.action == "close") {
-      event.notification.close();
+    if (e.action === "close") {
+      e.notification.close();
     }
   },
   false
