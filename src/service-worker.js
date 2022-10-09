58ed282adf3bb83548aa2e0d080861a669fa239e
Francesco Bondi <francesco.bondi02@gmail.com>
2022-10-09 19:39:58 +0200

    SW

--- src/service-worker.js
+++ src/service-worker.js
@@ -109,7 +109,7 @@ self.addEventListener(
     // Get all the Window clients
 
     e.waitUntil(
-      clients.matchAll({ type: "window" }).then((clientsArr) => {
+      self.clients.matchAll({ type: "window" }).then((clientsArr) => {
         // If a Window tab matching the targeted URL already exists, focus that;
         const hadWindowToFocus = clientsArr.some((windowClient) =>
           windowClient.url === e.notification.data.url
@@ -118,7 +118,7 @@ self.addEventListener(
         );
         // Otherwise, open a new tab to the applicable URL and focus it.
         if (!hadWindowToFocus)
-          clients
+          self.clients
             .openWindow(e.notification.data.url)
             .then((windowClient) =>
               windowClient ? windowClient.focus() : null
