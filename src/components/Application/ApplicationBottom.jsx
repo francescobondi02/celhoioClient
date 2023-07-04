36f11174ba0366f52b1b2533b0c55340a69143b6
Andrea Bondi <anbondi@gmail.com>
2023-07-04 15:11:07 +0200

    Cambiato link su facebook

--- src/components/Application/ApplicationBottom.jsx
+++ src/components/Application/ApplicationBottom.jsx
@@ -62,7 +62,7 @@ export default function ApplicationBottom(props) {
         <BottomNavigationAction label="Richieste" icon={<AddComment />} aria-multiline="true"/>
         <BottomNavigationAction label="Impostazioni" icon={<Settings />} />
         <BottomNavigationAction label="Installa WebAPP" icon={<Download />} />
-        <BottomNavigationAction label="BLUCCINO P.Iva 02307050399 REA RA 190151" href="http://www.bluccino.com/" target="_blank" />
+        <BottomNavigationAction label="BLUCCINO P.Iva 02307050399 REA RA 190151" href="https://www.facebook.com/celhoio.it" target="_blank" />
         
         
       </BottomNavigation>
